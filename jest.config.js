ed076a45508b086ed0fa67d8084a90ab7cb1e002
Matthew Goluba <mkgoluba@outlook.com>
2024-08-09 01:52:42 -0500

    fix jest

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,21 @@
-module.exports = {
+export default {
   preset: 'ts-jest',
   testEnvironment: 'node',
   roots: ['<rootDir>/src'],
   testMatch: ['**/__tests__/**/*.ts?(x)', '**/?(*.)+(spec|test).ts?(x)'],
   transform: {
-    '^.+\\.tsx?$': 'ts-jest',
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        useESM: true,
+      },
+    ],
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+    '^goobs-testing$': '<rootDir>/node_modules/goobs-testing/dist/index.js',
+  },
+  extensionsToTreatAsEsm: ['.ts', '.tsx'],
+  transformIgnorePatterns: ['node_modules/(?!(goobs-testing)/)'],
 };
